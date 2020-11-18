100ba98991a607f3d26cc34d33f684d3f24ba055
SalmaLMDN <74625958+SalmaLMDN@users.noreply.github.com>
2020-11-18 20:56:54 +0100

    Les fcts afficher et ajouter item

--- hash.cpp
+++ hash.cpp
@@ -29,6 +29,36 @@ int hash1::HASH(string cle)
     return index;
 }
 
+void hash1::afficherItem(){
+    for (int i = 0; i<tailleTableau; i++){
+        cout << "****   " << ;
+        cout << "Le nom: "<< HashTable[i]->nom << "//";
+        cout << "Telephone: "<< HashTable[i]->tel;
+         cout << "   ****" << endl;
+    }
+}
+
+ void hash1::ajouterItem(string nom, string tel){
+    int index;
+    index = HASH(nom);
+
+    if (HashTable[index]->nom == "vide"){
+        HashTable[index]->nom = nom;
+        HashTable[index]->tel = tel;
+        HashTable[index]->next = NULL;
+    } else {
+        item* p = HashTable[index];
+        item* newItem = new item;
+        newItem->nom = nom;
+        newItem->tel = tel;
+        newItem->next = NULL;
+        while (p->next != NULL){
+            p = p->next;
+        }
+        p->next = newItem;
+    }
+ }
+
 hash1::hash1(){
      for (int i = 0; i<tailleTableau; i++){
         HashTable[i] = new item;
