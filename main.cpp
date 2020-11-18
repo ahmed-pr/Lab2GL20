e220e56924f221b3cace403e04c03c0ce0e240fb
SalmaLMDN <74625958+SalmaLMDN@users.noreply.github.com>
2020-11-18 22:31:47 +0100

    Main update

--- main.cpp
+++ main.cpp
@@ -5,12 +5,32 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
     hash1 hashObj;
     int index;
     index = hashObj.HASH("Kerzazi");
-
+     int opt;
+    string key;
+    while (1)
+    {
+        cout << " 1. AddItem\t 2.DisplayItem \t 3.Search \t 4.Exit " << endl;
+        cin >> opt;
+        switch (opt)
+        {
+        case 1:
+            hashObj.ajouterItem();
+            break;
+        case 2:
+            hashObj.afficherItem();
+            break;
+        case 3:
+            hashObj.chercherItem();
+            break;
+        case 4:
+            exit(0);
+        }
+    }
 
     return 0;
 }
