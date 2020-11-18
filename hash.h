c0d4fc9576e24c394d8e99431f7ee8733122ea15
m3dani99 <56238389+m3dani99@users.noreply.github.com>
2020-11-18 23:13:39 +0100

    Revert "Revert "Merge pull request #1 from ahmed-pr/fonctions_chercherItem_supprimerIterm""
    This reverts commit a3ef50c435d09670a985a2121e340579da2b5adf.

--- hash.h
+++ hash.h
@@ -30,8 +30,8 @@ class hash1{
 
         //int  nombreItems(int index);
         //void afficherItemsDansIndex(int index); // les items qui ont le même resultat de hashage et qui sont empilés
-        //void chercherItem(string nom);
-        //void supprimerItem(string nom);
+        void chercherItem(string nom);
+        void supprimerItem(string nom);
 
 //        Un problème d’un entrevue chez Google.
 //        Soit une fonction qui prend comme paramètre une chaine de caractères et retourne le premier caractère récurrent dans la chaine.
