009e6133ebd19a2799c8793abcdb81790cea3ed6
SalmaLMDN <74625958+SalmaLMDN@users.noreply.github.com>
2020-11-19 01:59:33 +0100

    Last update

--- main.cpp
+++ main.cpp
@@ -16,5 +16,14 @@ int main(int argc, char const *argv[])
     hashObj.ajouterItem("Salma", "0654231436");
     cout<<"--------------Items-------------------"<<endl;
     hashObj.afficherItem();
+    hashObj.chercherItem("Khalil");
+    hashObj.chercherItem("Khalil");
+    cout<<"Le nombre d'Items dans l'indexe 4 est: "<<hashObj.nombreItems(4)<<endl;
+    cout<<"Items existant dans l'indexe 4 : "<<endl;
+    hashObj.afficherItemsDansIndex(3);
+    cout<<"On va proceder a supprimer l'item de nom khalil........"<<endl;
+    hashObj.supprimerItem("Khalil");
     return 0;
 }
+
+
